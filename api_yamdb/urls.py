a918e53cdf0057388044906ed3ccdeb6dced0d6a
Evans Pauliuts <pauliutsevans@gmail.com>
2021-09-14 09:54:08 +0300

    Финальный проект спринта: Continuous Integration проекта YaMDB | add template redoc

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
 from drf_yasg import openapi
+from django.views.generic import TemplateView
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
 
@@ -20,8 +21,8 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path(
         'redoc/',
-        schema_view.with_ui('redoc', cache_timeout=0),
-        name='schema-redoc'
+        TemplateView.as_view(template_name='redoc.html'),
+        name='redoc'
     ),
     path('swagger/', schema_view.with_ui(
         'swagger',
