7ae5ba14d5408774ffcafd79822c0e77bcb85818
Evans Pauliuts <pauliutsevans@gmail.com>
2021-09-14 09:25:03 +0300

    Финальный проект спринта: Continuous Integration проекта YaMDB | fix urls patterns redoc

--- api_yamdb/urls.py
+++ api_yamdb/urls.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from django.urls import include, path, re_path
-from django.views.generic import TemplateView
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
 from rest_framework import permissions
@@ -21,8 +20,8 @@ urlpatterns = [
     path('api/', include('api.urls')),
     path(
         'redoc/',
-        TemplateView.as_view(template_name='redoc.html'),
-        name='redoc'
+        schema_view.with_ui('redoc', cache_timeout=0),
+        name='schema-redoc'
     ),
     path('swagger/', schema_view.with_ui(
         'swagger',
